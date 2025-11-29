bf63ad2bc8c88104d64fc62908cebf65147bbdf9
Janis <janisherrmann@web.de>
2025-11-29 17:50:42 +0100

    Added Callbacks its now interactive

--- app.py
+++ app.py
@@ -32,7 +32,16 @@ app.layout = html.Div(children=[
     # Horizontal Pane
     html.Div(children = [
         #Sidebar
-        html.Div("Here we will put the Sidebar", style={"background" : "#1C0D0D", "width" : "200px"}), #Fixed width sidebar
+        html.Div(children = [
+            dcc.Tabs(id="sidebar_tabs", value="80s", children=[
+                dcc.Tab(label="80s", value="80s"),
+                dcc.Tab(label="90s", value="90s"),
+                dcc.Tab(label="00s", value="00s"),
+                dcc.Tab(label="10s", value="10s"),
+            ])
+    ], 
+             style={"background" : "#3D2C2C", "flexDirection" : "row"}), #Sidebar style
+        
         #Main content area
         html.Div(id="content_area", children = "Here we will put the Main content area", style={"background" : "#5B5757", "flex" : "1"}) #Flex 1 to take up all remaining space,
     ],
@@ -46,6 +55,20 @@ app.layout = html.Div(children=[
 #Options
 , style={"display" : "flex", "flexDirection" : "column", "height" : "100vh", "width" : "100vw" } #currently takes 100% of avaialble viewport height This might be stupid
 )
+
+
+# Callback to update content area based on selected tab
+@app.callback(
+    Output(component_id="content_area", component_property="children"),
+    Input(component_id="topbar_tabs", component_property="value"),
+    Input(component_id="sidebar_tabs", component_property="value"),
+)
+# If I remeber correctly the order of the parameters is the same as the order of the Inputs
+# Its a bit stupid
+def render_content(topbar_tab_value, sidebar_tab_value):
+    # This function takes the Input value as an argument
+    return f"You selected Topbar Tab: {topbar_tab_value} and Sidebar Tab: {sidebar_tab_value}"
+
 
 
 
